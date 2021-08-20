f99b8ff0e1f38b492477fb73fd209c9e6566ae5f
SamGman1 <88645353+SamGman1@users.noreply.github.com>
2021-08-21 00:49:41 +1000

    added some stuff and fixed the fpc

---
+++ Assets/PauseMenuFinal.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuFinal : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+
+    public GameObject pauseMenuUI;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+
+                Pause();
+            }
+
+        }
+
+    }
+
+
+    public void Resume()
+    {
+
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        GamePaused = false;
+
+    }
+
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        GamePaused = true;
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+
+
+    }
+
+
+
+}
